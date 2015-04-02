e529ff62d94d00e63b2434a722839b5687b67f40
Vastri <vastri@vastri.com>
2015-04-02 06:58:24 +0000

    Initial commit. A hello-world program.

---
+++ ratom.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""The main module for ratom."""
+
+def main():
+    """The main function of this module."""
+    print('Hello World!')
+
+if __name__ == '__main__':
+    main()
